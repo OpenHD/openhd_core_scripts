cdccac7d4d89d1987a2fbd64d44d2216e208eac6
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-02-04 20:41:48 +0100

    Update ohd_camera_setup.sh

--- ohd_camera_setup.sh
+++ ohd_camera_setup.sh
@@ -137,13 +137,13 @@ if [[ "$supported_platform" == true ]]; then
         echo "This Platform is Rockchip based and a RK3566 SOC"
         if apt list --installed | grep -q "u-boot-radxa-zero3"; then
         line=$(grep -n "fdtdir /usr/lib/linux-image-5.10.160-radxa-rk356x/" /boot/extlinux/extlinux.conf | cut -d: -f1)
-        echo "test"
         if [[ -n $line ]]; then
                 echo "true"
             # Check if the line after the matched line starts with "append"
             next_line=$(awk "NR==$line+1" /boot/extlinux/extlinux.conf)
             if [[ $next_line == append* ]]; then
                 # Insert "hello world" between the matched line and the "append" line
+                echo "true true blue"
                 sed -i "$line a hello world" /boot/extlinux/extlinux.conf
             fi
         fi
