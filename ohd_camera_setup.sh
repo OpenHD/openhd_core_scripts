9ecca4637a6fb8a67078c9659cff7b1cd1695c72
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-02-04 16:43:03 +0100

    refactor

--- ohd_camera_setup.sh
+++ ohd_camera_setup.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
-#script to setup OpenHD Cameras
+# Script to setup OpenHD Cameras
 
-#Raspberry
-# Look for the version of Raspberry Pi running
+# Determine Raspberry Pi board type
 uname_output=$(uname -a)
 kernel_version=$(echo "$uname_output" | awk '{print $3}')
 kernel_type=$(echo "$kernel_version" | awk -F '-' '{print $2}')
 
 if [[ "$kernel_type" == "v7l+" ]]; then
-  # kms
-  board_type="rpi_4_"
+    board_type="rpi_4_"
 elif [[ "$kernel_type" == "v7+" ]]; then
-  # fkms
-  board_type="rpi_3_"
+    board_type="rpi_3_"
 fi
 
-  # Look for the camera option selected by the user
-  output=""
-  configFileContent=$(</boot/openhd/camera.txt)
+# Read camera configuration from file
+config_file="/boot/openhd/camera.txt"
+config_file_content=$(<$config_file)
 
+# Define camera types
+case $config_file_content in
+    0) cam_type="X_CAM_TYPE_DUMMY_SW" ;;
+    1) cam_type="X_CAM_TYPE_USB" ;;
+    2) cam_type="X_CAM_TYPE_EXTERNAL" ;;
+    3) cam_type="X_CAM_TYPE_EXTERNAL_IP" ;;
+    4) cam_type="X_CAM_TYPE_DEVELOPMENT_FILESRC" ;;
+    10) cam_type="X_CAM_TYPE_RPI_MMAL_HDMI_TO_CSI" ;;
+    20) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V1_OV5647" ;;
+    21) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V2_IMX219" ;;
+    22) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_RPIF_V3_IMX708" ;;
+    23) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_RPIF_HQ_IMX477" ;;
+    30) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_SKYMASTERHDR" ;;
+    31) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_SKYVISIONPRO" ;;
+    32) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX477M" ;;
+    33) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX462" ;;
+    34) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX327" ;;
+    35) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX290" ;;
+    36) cam_type="X_CAM_TYPE_RPI_LIBCAMERA_ARDUCAM_IMX462_LOWLIGHT_MINI" ;;
+    50) cam_type="X_CAM_TYPE_RPI_V4L2_VEYE_2MP" ;;
+    51) cam_type="X_CAM_TYPE_RPI_V4L2_VEYE_CSIMX307" ;;
+    52) cam_type="X_CAM_TYPE_RPI_V4L2_VEYE_CSSC132" ;;
+    53) cam_type="X_CAM_TYPE_RPI_V4L2_VEYE_CSSC132" ;;
+    *) cam_type="Unknown" ;;
+esac
 
-# Append the filename to the output variable and write it in lowercase
-    filename=$(basename "$configFileContent" .txt | tr '[:upper:]' '[:lower:]')
-    output_org="$files"
-    output="$filename"
+# Echo camera type
+echo "Camera Type: $cam_type"
 
-#Echo camera
-echo $filename
+# Remove unnecessary lines from OpenHD config file
+sed -i '/#OPENHD_DYNAMIC_CONTENT_BEGIN#/,$d' /boot/openhd/rpi.txt
+cp /boot/config.txt /boot/config.txt.bak
 
-
-#Rockchip
-
+# Rockchip setup goes here
