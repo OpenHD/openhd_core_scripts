620664b0543972514cf02fe32781501b913ab238
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-03-04 12:37:05 +0100

    add openhd autologin

--- packaging/after-install.sh
+++ packaging/after-install.sh
@@ -1,2 +1,7 @@
 cp /usr/local/share/openhd_misc/motd /etc/motd
 cat /etc/motd
+
+if [[ $(uname -m) == "armv7l" || $(uname -m) == "armv7l+" ]]; then
+    echo "activating autologin on raspberry pi"
+    sudo sed -i 's/^ExecStart=-\/sbin\/agetty .*$/ExecStart=-\/sbin\/agetty --skip-login --login-options "-f root" %I 38400 linux/' /usr/lib/systemd/system/getty@.service
+fi
