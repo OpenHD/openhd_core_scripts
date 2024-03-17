dcf4a848b7de7c9b8010641629a03fdc615b232d
Pete A <peter.allenm@gmail.com>
2024-03-17 22:38:30 +0000

    Update scripts for the x20

---
+++ x20/runcam_v2/runcam_wb.sh
@@ -0,0 +1,20 @@
+
+CAM=0x22
+BUS=0
+
+# RUNCAM_Read_Write(camera_device, 0x0001b8, 0x020b007b); # Manual
+# RUNCAM_Read_Write(camera_device, 0x0001b8, 0x020b0079); # Auto
+
+# It is possible to manually set the WB, but this isn't implemented yet.
+
+if [ $1 -eq 0 ]; then
+    # Set auto WB
+     i2cset -y $BUS $CAM 0x12 0x00 0x01 0xb8 0x02 0x0b 0x00 0x79 i
+elif [ $1 -eq 1 ]; then
+    # Manual (fix at current value)
+     i2cset -y $BUS $CAM 0x12 0x00 0x01 0xb8 0x02 0x0b 0x00 0x7b i
+else
+	echo "Valid values of white balance are: "
+    echo "0 (auto) and 1 (fix to current)"
+fi
+
