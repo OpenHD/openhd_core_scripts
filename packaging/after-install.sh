fa1b404223a56033be2173b0b2a25a60617de310
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-05 12:49:46 +0200

    remove x20 stuff from everything not x20

--- packaging/after-install.sh
+++ packaging/after-install.sh
@@ -5,3 +5,8 @@ if [ -d /home/pi ]; then
     echo "activating autologin on raspberry pi"
     sudo sed -i 's/^ExecStart=-\/sbin\/agetty .*$/ExecStart=-\/sbin\/agetty --skip-login --login-options "-f root" %I 38400 linux/' /usr/lib/systemd/system/getty@.service
 fi
+
+if [ ! -e "/usr/local/share/openhd_platform/x20" ]; then
+    rm -Rf /usr/local/bin/x20/
+    systemctl disable temperature_guardian.service
+fi
