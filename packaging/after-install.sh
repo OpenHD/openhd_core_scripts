3193b3918b1b35f6ef4915cb195593cf2bb939ec
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-07 13:59:17 +0200

    Update after-install.sh

--- packaging/after-install.sh
+++ packaging/after-install.sh
@@ -6,7 +6,7 @@ if [ -d /home/pi ]; then
     sudo sed -i 's/^ExecStart=-\/sbin\/agetty .*$/ExecStart=-\/sbin\/agetty --skip-login --login-options "-f root" %I 38400 linux/' /usr/lib/systemd/system/getty@.service
 fi
 
-if [ ! -e "/usr/local/share/openhd_platform/x20" ]; then
+if [ ! -e "usr/lib/modules/5.8.0/kernel/drivers/meedia/i2c/HdZero3.ko" ]; then
     echo "Non Custom Installation Found"
     rm -Rf /usr/local/bin/x20/
     systemctl disable temperature_guardian.service
