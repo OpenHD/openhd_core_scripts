7f3537e3b7c77b4eb7fe47a49c88d743a9f81c8c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-12 03:08:57 +0200

    Update led_sys.sh

--- led_sys.sh
+++ led_sys.sh
@@ -5,16 +5,16 @@ COLOR=$2
 MODIFIER=$3
 DEBUG=$4
 
-# #Create a Lock file so this isn't executed multiple times.
-# if [ -d "/tmp/led.lock" ]; then
-#     pid=$(cat /tmp/led.lock)
-#     if [ -n "$pid" ]; then
-#         kill -9 "$pid"
-#         rm /tmp/led.lock
-#     fi
-# else
-#     echo $$ > /tmp/led.lock
-# fi
+#Create a Lock file so this isn't executed multiple times.
+if [ -d "/tmp/led.lock" ]; then
+    pid=$(cat /tmp/led.lock)
+    if [ -n "$pid" ]; then
+        kill -9 "$pid"
+        rm /tmp/led.lock
+    fi
+else
+    echo $$ > /tmp/led.lock
+fi
 
 debugMessage() {
     if [ "$DEBUG" == "debug" ]; then
