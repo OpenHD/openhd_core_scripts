f29770a5b30408a983284d52ecb5575e4d409732
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-04-14 20:53:38 +0200

    Update flash_emmc.sh

--- flash_emmc.sh
+++ flash_emmc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# OpenHD untility to write images to internal flash
+# OpenHD utility to write images to internal flash
 # Works on Radxa boards only
 # X20, CM3, Zero3W(E), CM5, Rock5A Rock5B
 
@@ -22,29 +22,29 @@ if [ -d /sys/class/leds/openhd-x20dev ]; then
     debugMessage "Platform: X20"
 elif [ ! -d /sys/class/leds/user-led/brightness ]; then
     PLATFORM="cm3"
-    debugMessage "Platform: X20"
-elif [ ! -d /sys/class/leds//board-led ]; then
+    debugMessage "Platform: CM3"
+elif [ ! -d /sys/class/leds/board-led ]; then
     PLATFORM="zero3"
-    debugMessage "Platform: zero3"
+    debugMessage "Platform: Zero3"
 fi
 
 # Detect which partition is currently used
 PARTITION=$(df -h / | awk 'NR==2 {print $1}')
 CARD=$(df --output=source / | tail -n 1 | cut -d'p' -f1)
-if [ $CARD == "/dev/mmcblk0" ]; then
+if [ "$CARD" == "/dev/mmcblk0" ]; then
     TARGET="/dev/mmcblk0"
-elif [ $CARD == "/dev/mmcblk0" ]; then
+elif [ "$CARD" == "/dev/mmcblk1" ]; then
     TARGET="/dev/mmcblk1"
 else 
-    exit:1
+    exit 1
+fi
 
 # Debug output
 DEBUG="debug"
 debugMessage "____ Platform: $PLATFORM ____"
 debugMessage "____ Partition: $PARTITION ____"
 debugMessage "____ Memory Card: $CARD ____"
-debugMessage "____ Memory Card: $TARGET ____"
-
+debugMessage "____ Target: $TARGET ____"
 
 # Set window/text color
 export NEWT_COLORS='
@@ -55,18 +55,17 @@ textbox=white,black
 button=white,black
 emptyscale=,black
 fullscale=,white
-' \
+'
 
 # Main Function of this script 
 led off
 led manual all 2 &
 
-# pv -n /opt/additionalFiles/emmc.img | dd of=/dev/mmcblk0 bs=128M conv=notrunc,>
-# echo "please reboot or powerdown the system now"
-
+pv -n /opt/additionalFiles/emmc.img | dd of="$TARGET" bs=128M conv=notrunc
+echo "Please reboot or power down the system now"
 
 mkdir -p /media/new
-mount /dev/mmcblk0p1 /media/new
+mount "$TARGET"p1 /media/new
 cp -r /boot/openhd/* /media/new/openhd/
 whiptail --msgbox "Please reboot your system now" 10 40
 led off
