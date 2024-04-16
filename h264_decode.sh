25ab11bbe8669c837cb7940999cbadd0055801f3
Consti10 <geierconstantinabc@gmail.com>
2024-04-16 14:54:51 +0200

    experiment - x20 option
    NOTE: Breaks rpi comp., only for testing

--- h264_decode.sh
+++ h264_decode.sh
@@ -17,7 +17,7 @@ function execute_FPVueVideoDecode {
     gst-launch-1.0 udpsrc port=5600 caps='application/x-rtp, payload=(int)96, clock-rate=(int)90000, media=(string)video, encoding-name=(string)H264' ! rtph264depay ! h264parse config-interval=1 ! video/x-h264,stream-format=byte-stream,alignment=au ! fdsink fd=1 | fpvue  --rmode 10
 }
 function execute_FPVueVideoDecode2 {
-  fpvue  --gst-udp-port 5600 --rmode 5
+  fpvue  --gst-udp-port 5600 --rmode 5 --x20
 }
 
 # Detect Platform
