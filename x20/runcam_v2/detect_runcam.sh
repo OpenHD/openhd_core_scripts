d378b252c47aeeb7be7ba7bc94811c4770667f1d
Pete A <peter.allenm@gmail.com>
2024-04-28 23:01:12 +0100

    Add script to detect managed runcam cameras

---
+++ x20/runcam_v2/detect_runcam.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+v2=`i2cdetect -y 0 |grep 20 | grep 24`
+v2=`i2cdetect -y 0 |grep 20 | grep 22`
+v1=`i2cdetect -y 0 |grep 20 | grep 21`
+nano90=`i2cdetect -y 0 |grep 20 | grep 23`
+
+
+#echo "v3=${#v3}, v1=${#v1}"
+if (( ${#v3} > 1 )); then
+        echo "Detected RUNCAM V3"
+        exit 3
+elif (( ${#v1} > 1 )); then
+        echo "Detected RUNCAM V1"
+        exit 1
+elif (( ${#v2} > 1 )); then
+        echo "Detected RUNCAM V2"
+        exit 2
+elif (( ${#nano90} > 1 )); then
+        echo "Detected RUNCAM NANO 90"
+        exit 90
+else
+        echo "No Runcam detected, presuming hdzero unmanaged camera"
+        exit 0
+fi
