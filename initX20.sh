e2cb96b2a5248556a165c16c392a25b42adf528c
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-26 20:59:16 +0200

    testing 20 amp command

--- initX20.sh
+++ initX20.sh
@@ -10,7 +10,7 @@ touch /usr/local/share/openhd_platform/x20/hdzero
 
 sleep 10
 depmod -a
-modprobe 88XXau_ohd
+modprobe 88XXau_ohd rtw_amplifier_type_2g=0 rtw_amplifier_type_5g=192 rtw_RFE_type=1 rtw_TxBBSwing_2G=0 rtw_TxBBSwing_5G=3
 modprobe HdZero
 gst-launch-1.0 -ve v4l2src device=/dev/video0 num-buffers=72 ! video/x-raw,width=1280,height=720,framerate=60/1,format=NV12 ! cedar_h264enc keyint=12 bitrate=16000 !  video/x-h264 ! h264parse ! matroskamux ! filesink location="/home/openhd/$fname" >& /home/openhd/GStreamerLog.txt
 
