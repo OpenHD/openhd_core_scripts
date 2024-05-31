cf1833e178aa982d8a5f0713da6447c3ee4fd995
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-31 23:07:21 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# Loop indefinitely
 while true; do
-  # Check if the directory exists
   if [ -d "/tmp/openhd_status" ]; then
-    # Print "Hello, World!"
-    echo "Hello, World!"
+    wall -n "Hello world"
   else
-    # Exit the loop if the directory does not exist
     break
   fi
-  
-  # Sleep for a short time to prevent excessive CPU usage
-  sleep 1
+    sleep 10
 done
-
-echo "Directory /tmp/openhd_status does not exist. Exiting."
