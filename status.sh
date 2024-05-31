614271c561222b2bb9d8d5563b9083e24abe233b
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-05-31 23:34:12 +0200

    Update status.sh

--- status.sh
+++ status.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+lookForWarning() {
+    if [ -d "/tmp/openhd_status" ]; then
+        for dir in /tmp/openhd_status/*/; do
+            if [ -d "$dir" ] && [ "$(basename "$dir")" == "openhd" ] && [ -f "$dir/warning" ]; then
+                cat "$dir/warning"
+            fi
+        done
+    else
+        return 1
+    fi
+}
+
 while true; do
-  if [ -d "/tmp/openhd_status" ]; then
-    wall -n "Hello world"
-  else
-    break
-  fi
-    sleep 10
+    lookForWarning || break
+    sleep 5
 done
