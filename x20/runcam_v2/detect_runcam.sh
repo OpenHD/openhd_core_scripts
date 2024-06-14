903063f31dcf49be8e91d91db9fdb732b022cac6
Pete A <peter.allenm@gmail.com>
2024-06-14 14:00:28 +0100

    Use echo rather than return types as that's what openhd expects. Also add a sleep is i2c takes a while to start up

--- x20/runcam_v2/detect_runcam.sh
+++ x20/runcam_v2/detect_runcam.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sleep 3
 v2=`i2cdetect -y 0 |grep 20 | grep 24`
 v2=`i2cdetect -y 0 |grep 20 | grep 22`
 v1=`i2cdetect -y 0 |grep 20 | grep 21`
@@ -7,16 +8,20 @@ nano90=`i2cdetect -y 0 |grep 20 | grep 23`
 
 #echo "v3=${#v3}, v1=${#v1}"
 if (( ${#v3} > 1 )); then
-        echo "Detected RUNCAM V3"
+        #echo "Detected RUNCAM V3"
+        echo 3
         exit 3
 elif (( ${#v1} > 1 )); then
-        echo "Detected RUNCAM V1"
+        #echo "Detected RUNCAM V1"
+        echo 1
         exit 1
 elif (( ${#v2} > 1 )); then
-        echo "Detected RUNCAM V2"
+        #echo "Detected RUNCAM V2"
+        echo 2
         exit 2
 elif (( ${#nano90} > 1 )); then
-        echo "Detected RUNCAM NANO 90"
+        #echo "Detected RUNCAM NANO 90"
+        echo 90
         exit 90
 else
         echo "No Runcam detected, presuming hdzero unmanaged camera"
