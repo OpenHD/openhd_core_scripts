3d82301e9ad442c370103f804c5d19bafbca8786
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-19 15:57:19 +0200

    cleanup

--- initRock.sh
+++ initRock.sh
@@ -52,16 +52,6 @@ fi
 if [[ -f "/boot/openhd/rock-rk3566.txt" ]]; then
     echo "Running on a rk3566 "
 
-    if [ -e /boot/openhd/openhd ]; then
-        #Annoying hack to get files from mounted partition 
-        mkdir /boot/temp
-        mv /boot/openhd/openhd/* /boot/temp
-        rm -rf /boot/openhd/openhd
-        mv /boot/temp/* /boot/openhd/
-        rm -Rf /boot/temp
-        sleep 1
-    fi
-
     if [ -e /boot/openhd/air.txt ]; then 
         if [ -e /boot/openhd/camera1.txt ] && [ ! -e /boot/openhd/camera.txt ]; then
             #quite hacky now, but better then nothing
@@ -78,22 +68,7 @@ if [[ -f "/boot/openhd/rock-rk3566.txt" ]]; then
         fi
     exit 0
     fi
-    # config_file=$(find /boot/openhd/ -type f -name 'IMX*')
-    
-    # if [[ -n "$config_file" ]]; then
-    #     if [[ "$config_file" == *"/708"* ]]; then
-    #         cp "/boot/openhd/rock3_camera_configs/zero3w/imx708.conf" "/boot/extlinux/extlinux.conf"
-    #         echo "$config_file written"
-    #     elif [[ "$config_file" == *"/IMX462"* ]]; then
-    #         cp "/boot/openhd/rock3_camera_configs/zero3w/imx462.conf" "/boot/extlinux/extlinux.conf"
-    #         echo "$config_file written"
-    #     else
-    #         echo "No Camera configured"
-    #     fi
-    # else
-    #     echo "Config file not found"
-    # fi
-
+   
     if [[ -e "/boot/openhd/resize.txt" ]]; then
     echo "resizing started"
     rm /boot/openhd/resize.txt
