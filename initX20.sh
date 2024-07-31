c9a7b2f16f1d843d3b5c80cd4a2f1bfeaef9aec2
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-01 00:45:42 +0200

    Update initX20.sh

--- initX20.sh
+++ initX20.sh
@@ -17,7 +17,7 @@ depmod -a
 modprobe HdZero
 if [ ! -f /usr/local/share/openhd_platform/x20/firstboot_done ]; then
     echo "started once" >> /external/log2.txt
-    rm /run/openhd/hold.pid
+    touch /usr/local/share/openhd_platform/x20/firstboot_done
     reboot
 else
     ls -a /usr/local/share/openhd_platform/x20/ >> /external/log2.txt
