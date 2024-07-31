0d86413f1b215d76fb31170471dcbba635adb6ce
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-07-31 23:53:57 +0200

    kmp

--- initX20.sh
+++ initX20.sh
@@ -8,6 +8,7 @@ mkdir -p /usr/local/share/openhd_platform/wifi_card_type/88xxau/
 touch /usr/local/share/openhd_platform/wifi_card_type/88xxau/custom
 touch /usr/local/share/openhd_platform/x20/hdzero
 
+
 echo 1 >/sys/class/leds/openhd-x20dev\:red\:usr/brightness
 echo 0 >/sys/class/leds/openhd-x20dev\:green\:usr/brightness
 echo 0 >/sys/class/leds/openhd-x20dev\:blue\:usr/brightness
@@ -15,11 +16,12 @@ echo 0 >/sys/class/leds/openhd-x20dev\:blue\:usr/brightness
 depmod -a
 modprobe HdZero
 if [ ! -f /usr/local/share/openhd_platform/x20/firstboot_done ]; then
-    touch /usr/local/share/openhd_platform/x20/firstboot_done
-    touch /external/log2.txt
     echo "started once" >> /external/log2.txt
     rm /run/openhd/hold.pid
     reboot
+else
+    ls -a /usr/local/share/openhd_platform/x20/ >> /external/log2.txt
+    echo "Already started before" >> /external/log2.txt
 fi
 
 echo 0 >/sys/class/leds/openhd-x20dev\:red\:usr/brightness
