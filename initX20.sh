95ca8b05e1dfd345bade4ce9a530f509f95d160e
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-25 22:37:24 +0200

    resize config/video part x20

--- initX20.sh
+++ initX20.sh
@@ -18,6 +18,15 @@ modprobe HdZero
 if [ ! -f /usr/local/share/openhd_platform/x20/firstboot_done ]; then
     echo "started once" >> /external/log2.txt
     touch /usr/local/share/openhd_platform/x20/firstboot_done
+    #resize part
+        mkdir -p /run/openhd/
+        touch /run/openhd/hold.pid
+        echo "resizing partition"
+        parted /dev/mmcblk0 --script resizepart 3 100%
+        sudo rm /external/openhd/resize.txt
+        mkdir -p /tmp/test
+        mount /dev/mmcblk0p3 /tmp/test
+        touch /tmp/test/external_video_part.txt
     reboot
 else
     ls -a /usr/local/share/openhd_platform/x20/ >> /external/log2.txt
